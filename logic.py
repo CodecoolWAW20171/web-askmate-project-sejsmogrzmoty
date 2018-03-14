4a62ef763dde55ab50be7be19d1280c6f0545218
Bartosz Listwan <bartoszlistwan@gmail.com>
2018-03-14 14:58:43 +0100

    few functions added

--- logic.py
+++ logic.py
@@ -3,20 +3,25 @@ import util
 
 
 # ----- Constants -----
-QSTN_HEADERS = []
-ANSW_HEADERS = []
+QSTN_HEADERS = ['id', 'submisson_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+ANSW_HEADERS = ['id', 'submisson_time', 'vote_number', 'question_id', 'message', 'image']
 
 
 def get_all_questions():
-    pass
+    return persistence.get_data_from_file("question.csv")
 
 
 def get_answers_to_question(qstn_id):
-    pass
+    complete_data = persistence.get_data_from_file("answer.csv")
+    listed_answers = []
+    for data in complete_data:
+        if data['question_id'] == qstn_id:
+            listed_answers.append(data)
+    return listed_answers
 
 
 def add_new_question(question):
-    pass
+    return persistence.write_data_to_file(question, "question.txt")
 
 
 def modify_question(qstn_id, question):
