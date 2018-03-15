39900b9660759159caf495c3d26fe466d0b16baa
Matixk <matixk73@gmail.com>
2018-03-15 09:59:05 +0100

    Data handler function added.
    Added possibility to write data to csv file with base64 encoding and get data from file with base64 decoding.

--- logic.py
+++ logic.py
@@ -3,8 +3,8 @@ import util
 
 
 # ----- Constants -----
-QSTN_HEADERS = []
-ANSW_HEADERS = []
+QSTN_HEADERS = ["id", "submisson_time", "view_number", "vote_number", "title", "message", "image"]
+ANSW_HEADERS = ["id", "submisson_time", "vote_number", "question_id", "message", "image"]
 
 
 def get_all_questions():
@@ -43,3 +43,26 @@ def generate_new_id(data):
 
 def find_id_index(data, id_):
     pass
+
+
+def code_string(dictionary, header, key):
+    """
+    Transcoding dictionary value to or from base64.
+
+    Args:
+        dictionary: dictionary 
+        header: Dictionary header
+        key: Type of cryptography
+
+    Returns:
+        Decoded/encoded string
+    """
+    if header in ["title", "message", "image"]:
+        if key == "encode":
+            return str(base64.b64encode(bytes(dictionary[header], "utf-8")))[2:-1]
+        elif key == "decode":
+            return base64.b64decode(bytes(dictionary[header], "utf-8")).decode("utf-8")
+        else:
+            raise ValueError("Wrong key!")
+    else:
+        return dictionary[header]
--- persistence.py
+++ persistence.py
@@ -2,20 +2,55 @@ import csv
 import os
 
 import util
-
+import base64
 
 # ----- Constants -----
-QSTN_FILE_PATH = ''
-ANSW_FILE_PATH = ''
+ANSW_FILE_PATH = os.getenv('ANSW_FILE_PATH') if 'ANSW_FILE_PATH' in os.environ else "sample_data/answer.csv"
+QSTN_FILE_PATH = os.getenv('QSTN_FILE_PATH') if 'QSTN_FILE_PATH' in os.environ else "sample_data/question.csv"
 
 
 # Get functions
 # ########################################################################
 def get_data_from_file(filename):
-    pass
+    """
+    Reads csv file and returns it as a list of dictionaries.
+    Lines are rows columns are separated by ","
+
+    Args:
+        file_name (str): name of file to read
+
+    Returns:
+        List of dictionaries read from a file.
+    """
+    result = []
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            for header in row:
+                print(header)
+                row[header] = code_string(row, header, "decode")
+            result.append(row)
+    return result
 
 
 # Write functions
 # ########################################################################
-def write_data_to_file(data, filename):
-    pass
+def write_data_to_file(data, filename, header):
+    """
+    Writes list of dictionaries into a csv file with base64 encoding.
+
+    Args:
+        file_name (str): name of file to write to
+        data: list of dictionaries to write to a file
+        header: Dictionary header
+
+    Returns:
+        None
+    """
+    with open(filename, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=header)
+        writer.writeheader()
+        for dictionary in data:
+            for key in dictionary:
+                dictionary[key] = code_string(dictionary, key, "encode")
+            writer.writerow(dictionary)
